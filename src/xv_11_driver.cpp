fdbfc5cf2a8dceaa658330c3e477003016c6256c
nhewitt99 <hewittna@oregonstate.edu>
2023-02-10 19:42:09 -0800

    Ported to Humble parameter style. Changed defaults.

--- src/xv_11_driver.cpp
+++ src/xv_11_driver.cpp
@@ -64,71 +64,59 @@
 // default xv11 dev name.  Suggest you use symbolic link for an easier to ricognize name on your system
 // example:  sudo ln -s /dev/yourTtyDevName /dev/ttyXV11
 // Here I assume you have setup symbolic link to your actual serial port tty driver to the lidar
-#define XV11_PORT_DEFAULT "/dev/ttyXV11"           // Serial device driver name (sym link to real dev)
+#define XV11_PORT_DEFAULT "/dev/ttyUSB0"           // Serial device driver name
 #define XV11_BAUD_RATE_DEFAULT 115200              // Serial baud rate
-#define XV11_FRAME_ID_DEFAULT "neato_laser"        // frame_id in LaserScan messages
+#define XV11_FRAME_ID_DEFAULT "laser_frame"        // frame_id in LaserScan messages
 #define XV11_FIRMWARE_VERSION_DEFAULT 2            // XV-11 firmware rev. 1 is oldest
 
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("xv11_laser");
+    auto node = rclcpp::Node::make_shared("xv11_laser");
+    node->declare_parameter("port", XV11_PORT_DEFAULT);
+    node->declare_parameter("baud_rate", XV11_BAUD_RATE_DEFAULT);
+    node->declare_parameter("frame_id", XV11_FRAME_ID_DEFAULT);
+    node->declare_parameter("firmware_version", XV11_FIRMWARE_VERSION_DEFAULT );
 
-  node->declare_parameter("port");
-  auto port_param      = rclcpp::Parameter("port", XV11_PORT_DEFAULT);
+    std::string port = node->get_parameter("port").as_string();
+    int baud_rate = node->get_parameter("baud_rate").as_int();
+    std::string frame_id = node->get_parameter("frame_id").as_string();
+    int firmware_number = node->get_parameter("firmware_version").as_int();
 
-  node->declare_parameter("baud_rate");
-  auto baud_rate_param = rclcpp::Parameter("baud_rate", XV11_BAUD_RATE_DEFAULT);
+    auto laser_pub = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", 10);
 
-  node->declare_parameter("frame_id");
-  auto frame_id_param  = rclcpp::Parameter("frame_id", XV11_FRAME_ID_DEFAULT);
+    // std_msgs::msg::UInt16 rpms;
+    boost::asio::io_service io;
 
-  node->declare_parameter("firmware_version");
-  auto firmware_param  = rclcpp::Parameter("firmware_version", XV11_FIRMWARE_VERSION_DEFAULT);
-    
-  node->get_parameter_or("port", port_param, port_param);
-  node->get_parameter_or("baud_rate", baud_rate_param, baud_rate_param);
-  node->get_parameter_or("frame_id", frame_id_param, frame_id_param);
-  node->get_parameter_or("firmware_version", firmware_param, firmware_param);
+    try {
+        xv_11_driver::XV11Laser laser(port, baud_rate, firmware_number, io);
 
-  std::string port     = port_param.value_to_string();
-  int baud_rate        = baud_rate_param.as_int();
-  std::string frame_id = frame_id_param.value_to_string();
-  int firmware_number  = firmware_param.as_int();
+        // auto motor_pub = node->create_publisher<std_msgs::UInt16>("rpms",1000);
 
-  auto laser_pub = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", 10);
+        while (rclcpp::ok()) {
+            sensor_msgs::msg::LaserScan *scan;
+            scan = new sensor_msgs::msg::LaserScan;
+            // sensor_msgs::LaserScan::Ptr scan(new sensor_msgs::LaserScan);
+            scan->header.frame_id = frame_id;
+            scan->header.stamp = rclcpp::Clock().now();   //  ROS was  Time::now();
+            laser.poll(scan);
+            laser_pub->publish(*scan);
 
-  // std_msgs::msg::UInt16 rpms;
-  boost::asio::io_service io;
+            //rpms.data=laser.rpms;
+            // motor_pub->publish(rpms);
 
-  try {
-    xv_11_driver::XV11Laser laser(port, baud_rate, firmware_number, io);
-
-    // auto motor_pub = node->create_publisher<std_msgs::UInt16>("rpms",1000);
-
-    while (rclcpp::ok()) {
-      sensor_msgs::msg::LaserScan *scan;
-      scan = new sensor_msgs::msg::LaserScan;
-      // sensor_msgs::LaserScan::Ptr scan(new sensor_msgs::LaserScan);
-      scan->header.frame_id = frame_id;
-      scan->header.stamp = rclcpp::Clock().now();   //  ROS was  Time::now();
-      laser.poll(scan);
-      laser_pub->publish(*scan);
-
-      //rpms.data=laser.rpms;
-      // motor_pub->publish(rpms);
-
+        }
+        laser.close();
+        return 0;
+    } catch (...) {
+        // TODO: how can we print out this exception instead
+        RCLCPP_ERROR(node->get_logger(), "Error instantiating laser object. Check correct port and baud rate!");
+        return -1;
     }
-    laser.close();
+
+
+    rclcpp::shutdown();
     return 0;
-  } catch (...) {
-    RCLCPP_ERROR(node->get_logger(), "Error instantiating laser object. Check correct port and baud rate!");
-    return -1;
-  }
-
-
-  rclcpp::shutdown();
-  return 0;
 }
